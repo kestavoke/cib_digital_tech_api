fb895f1c0d4f77faae21da1b88f6b30fe829a686
kestavoke <talktovoke@gmail.com>
2019-08-28 01:11:02 +0200

    Integrating Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,34 +1,36 @@
-#!groovy
-node {
+pipeline {
+    agent { any 'linux' }
+    stages {
 
-    stage('Smoke') {
-        try {
-            sh "mvn clean install"
-        } catch (err) {
-
-        } finally {
-            publishHTML (target: [
-                    reportDir: 'target/site/serenity',
-                    reportFiles: 'index.html',
-                    reportName: "Smoke tests report"
-            ])
+        stage ('Build') {
+            steps {
+                sh 'mvn clean package'
+                junit '**/target/surefire-reports/TEST-*.xml'
+                archiveArtifacts artifacts: 'target/*.jar', fingerprint: true
+            }
         }
-    }
-    stage('API') {
-        try {
-            sh "mvn clean install"
-        } catch (err) {
 
-        } finally {
-            publishHTML (target: [
-                    reportDir: 'target/site/serenity',
-                    reportFiles: 'index.html',
-                    reportName: "API tests report"
-            ])
+        stage('Smoke') {
+            steps {
+                sh 'mvn clean install'
+                publishHTML(target: [
+                        reportDir  : 'target/site/serenity',
+                        reportFiles: 'index.html',
+                        reportName : "Smoke tests report"
+                ])
+            }
         }
-    }
-    stage('Results') {
-        junit testResults: '**/target/failsafe-reports/*.xml', allowEmptyResults: true
-        archiveArtifacts '**target/**'
+        stage('API') {
+                sh 'mvn clean install'
+                publishHTML(target: [
+                        reportDir  : 'target/site/serenity',
+                        reportFiles: 'index.html',
+                        reportName : "API tests report"
+                ])
+            }
+        stage('Results') {
+            junit testResults: '**/target/failsafe-reports/*.xml', allowEmptyResults: true
+            archiveArtifacts '**target/**'
+        }
     }
 }
